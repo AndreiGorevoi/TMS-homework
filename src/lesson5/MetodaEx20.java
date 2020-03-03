c6b43451f756180196c492c1ff2d664cd655c47d
Andrei Gorevoi <18.andreigorevoi@gmail.com>
2020-03-03 21:41:16 +0300

    fix for 20 ex.

--- src/lesson5/MetodaEx20.java
+++ src/lesson5/MetodaEx20.java
@@ -9,26 +9,11 @@ public class MetodaEx20 {
         String str = "  Имеется строка с текстом. Вывести текст, составленный из последних букв всех слов.  ";
         char symbol;
         str= str.trim();
-        // Второй метод через split
-        for (String retval: str.split(" ")) {
-                char temp = retval.charAt(retval.length()-1);
-            if (temp!='.' && temp!='!' && temp!='?' && temp!='-' && temp!=';' && temp!=':'&& temp!=',') {
-                System.out.print(temp);
-            }else {
-                System.out.print(retval.charAt(retval.length()-2));
-            }
+        // Метод через split
+        String [] strArr = str.split("\\s*(\\s|,|\\||!|\\.)\\s*");
+        for (int i = 0; i <strArr.length ; i++) {
+            System.out.print(strArr[i].charAt(strArr[i].length()-1));
         }
-
-       /* for (int i = 0; i <str.length() ; i++) {
-            symbol=str.charAt(i);
-           if(symbol==' ' && (str.charAt(i-1)!='.'&& str.charAt(i-1)!=',' && str.charAt(i-1)!=';' && str.charAt(i-1)!=':'
-            && str.charAt(i-1)!='?' && str.charAt(i-1)!='!' && str.charAt(i-1)!='-')){
-               strOfLastChars+=str.charAt(i-1);
-           }
-            if(symbol=='.' || symbol=='!' || symbol=='?' || symbol=='-' ||symbol==';' || symbol==':'){
-               strOfLastChars+=str.charAt(i-1);
-           }
-        }*/
 
     }
 }
